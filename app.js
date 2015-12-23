844516c52fa2afb3165f2392b28af4df1dbb5a2c
Falcon Taylor-Carter <falconertc@colorado.edu>
2015-12-22 20:27:17 -0500

    Add basic communication between client and server

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use('/scripts', express.static(__dirname + '/node_modules/clientjs/dist/'));
+app.use('/scripts', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     //res.send('Hi');
     res.sendFile(__dirname + "/index.html");
-    //res.render('index.html');
 
     var ip = req.ip;
     console.log('User connected: %s', ip);
+});
+
+app.post("/", function(req, res) {
+    var OS = req.body.OS;
+    console.log(OS);
+    //return res.json({a: "a"}, 200);
+    res.end("Words");
 });
 
 var server = app.listen(3000, function() {
--- public/client.js
+++
@@ -1,6 +0,0 @@
-console.log("hello");
-
-var client = new ClientJS();
-
-var OS = client.getOS();
-console.log(OS);
