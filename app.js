a151ac1c8714a6ba42f30181a9f3fa34ac5e14e2
Falcon Taylor-Carter <falconertc@colorado.edu>
2015-12-22 19:52:28 -0700

    Lint files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var sha1 = require('js-sha1')
+var sha1 = require('js-sha1');
 var app = express();
 
 var registeredDevices = [];
@@ -9,28 +9,32 @@ app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + '/index.html');
 
 });
 
-app.post("/", function(req, res) {
+app.post('/', function(req, res) {
     var id = req.body.id;
     var hash = sha1(req.ip + id);
 
     var deviceVal = 0;
     /* Check if device is registered */
     registeredDevices.forEach(function(elem) {
-	if (elem.id === hash)
-          deviceVal = (++elem.count);
+        if (elem.id === hash)
+            deviceVal = (++elem.count);
     });
 
     if (deviceVal <= 0) {
-	var device = {id: hash, count: 1};
-	var index = registeredDevices.push(device);
-	console.log("Added device number: "+ (index + 1) + " id: "+hash);
+        var device = {
+            id: hash,
+            count: 1
+        };
+        var index = registeredDevices.push(device);
+        console.log('Added device number: ' + (index + 1) + ' id: ' + hash);
     }
 
-    var response = (deviceVal > 0 ? "Access count: " + deviceVal : "New device added!");
+    var response = (deviceVal > 0 ? 'Access count: ' +
+        deviceVal : 'New device added!');
     res.end(response);
 });
 
